06d5ea86e9bd9b15eebe28b4dcf7f1531c8f734d
hugo-franca <franca.hugo1@gmail.com>
2019-09-13 19:05:30 -0300

    t1

--- Trabalho1/main_normais_obj.m
+++ Trabalho1/main_normais_obj.m
@@ -37,14 +37,3 @@ end
 
 
 
-function vertices = anel(indice_v, V, V_corners, Corner_table)
-	vertices = [];
-
-	corners = V_corners{indice_v}; % Pega todos os corners deste vertice
-	for i_corner=1:length(corners) % Percorre cada um dos corners
-		corner = Corner_table{ corners(i_corner) }; % Obtem a estrutura  deste corner na table 
-		prox_corner = Corner_table{ corner.prox }; % Obtem a estrutura do proximo coner na table
-		i_v = prox_corner.vert; % Indice do vertice do prox corner
-		vertices(end+1, :) = V(i_v, :); % Adiciona o vertice na lista do anel
-	end
-end
--- Trabalho1/suavizacao_vertices.m
+++ Trabalho1/suavizacao_vertices.m
@@ -23,18 +23,6 @@ function V = suavizacao_vertices(V, F, V_corners, Corner_table, Normais)
 
 end
 
-function vertices = anel(indice_v, V, V_corners, Corner_table)
-	vertices = [];
-
-	corners = V_corners{indice_v}; % Pega todos os corners deste vertice
-	for i_corner=1:length(corners) % Percorre cada um dos corners
-		corner = Corner_table{ corners(i_corner) }; % Obtem a estrutura  deste corner na table 
-		prox_corner = Corner_table{ corner.prox }; % Obtem a estrutura do proximo coner na table
-		i_v = prox_corner.vert; % Indice do vertice do prox corner
-		vertices(end+1, :) = V(i_v, :); % Adiciona o vertice na lista do anel
-	end
-end
-
 function v = baricentro(verts)
 	n = size(verts, 1);
 	v = [0 0 0];
